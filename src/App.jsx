b268419d68c26ecf7e5607c1075717435d6a586a
El-Sankey <i.a.rivashernandez@gmail.com>
2025-05-22 00:33:05 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
 import './App.css'
+import Buttons from "./components/buttons.jsx";
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+
+    return (
+        <>
+            <header>
+                <h1>Handbags & Purses</h1>
+                <nav><Buttons/></nav>
+            </header>
+            <main>
+                <article>
+                    <span>
+                    </span>
+                    <img src="{}" alt=""/>
+                    <p>
+                    </p>
+                    <h4>
+                    </h4>
+                </article>
+
+                <article>
+                    <span>
+                    </span>
+                    <img src="" alt=""/>
+                    <p>
+                    </p>
+                    <h4>
+                    </h4>
+                </article>
+
+                <article>
+                    <span>
+                    </span>
+                    <img src="" alt=""/>
+                    <p>
+                    </p>
+                    <h4>
+                    </h4>
+                </article>
+
+                <article>
+                    <span>
+                    </span>
+                    <img src="" alt=""/>
+                    <p>
+                    </p>
+                    <h4>
+                    </h4>
+                </article>
+            </main>
+        </>
+
+    )
 }
 
 export default App
